760ecdf00ab08124f1a670d94cf6970682f2dd4a
Sigurd Kolltveit <sigurd.kolltveit@gmx.com>
2016-05-17 14:20:57 +0200

    Add print function

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::env;
 use std::fs::File;
 use std::io::BufReader;
 
+
 #[derive(Copy, Clone)]
 enum Slot {
     Empty,
@@ -47,22 +48,39 @@ impl SudokuPuzzle {
         Ok(puzzle)
     }
 
-    fn print() {
+    fn print(&self) {
+        let row_separator = ["-"; 9];
+        let top_bot_sep = row_separator.join("-");
+        println!("{}", top_bot_sep);
+        let mut formatted_rows = vec![];
+        for row in self.grid.iter() {
+            let mut str_row = "|".to_owned();
+            for &slot in row.iter() {
+                let s = match slot {
+                    Slot::Empty => " |".to_owned(),
+                    Slot::Filled(n) => format!("{}|", n),
+                };
+                str_row.push_str(&s);
+            }
+            str_row.push('\n');
+            formatted_rows.push(str_row);
+        }
+        let between_sep = format!("|{}\n", row_separator.join("+"));
+        print!("{}", formatted_rows.join(&between_sep));
+        println!("{}", top_bot_sep);
     }
 
 }
 
 
 fn main() {
+
     let stdin = stdin();
     let puzzle = match env::args().nth(1) {
-        Some(path) => SudokuPuzzle::new(BufReader::new(File::open(path).unwrap())),
-        None => SudokuPuzzle::new(stdin.lock()),
+        Some(path) => SudokuPuzzle::new(BufReader::new(File::open(path).unwrap())).unwrap(),
+        None => SudokuPuzzle::new(stdin.lock()).unwrap(),
     };
 
-    match puzzle {
-        Ok(_) => println!("Success!"),
-        Err(e) => println!("Error: {}", e),
-    }
-    println!("Hello, world!");
+    puzzle.print();
+
 }
