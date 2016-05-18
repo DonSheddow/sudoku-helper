94890565a3ce9c0f127102ccaa8abe82df99f12a
Sigurd Kolltveit <sigurd.kolltveit@gmx.com>
2016-05-19 00:34:31 +0200

    Add function to iterate over blocks

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,6 @@ type Grid = [[Slot; 9]; 9];
 
 type RowIterator<'a> = ::std::slice::Iter<'a, [Slot; 9]>;
 
-
 struct ColumnIterator<'a> {
     grid: &'a Grid,
     column: u8,
@@ -18,14 +17,14 @@ struct ColumnIterator<'a> {
 
 impl<'a> ColumnIterator<'a> {
     fn new(grid: &'a Grid) -> ColumnIterator<'a> {
-        ColumnIterator { grid: grid, column: 0}
+        ColumnIterator { grid: grid, column: 0 }
     }
 }
 
 impl<'a> Iterator for ColumnIterator<'a> {
     type Item = Vec<&'a Slot>;
 
-    fn next(&mut self) -> Option<Vec<&'a Slot>> {
+    fn next(&mut self) -> Option<Self::Item> {
 
         if self.column >= 9 {
             return None;
@@ -37,6 +36,44 @@ impl<'a> Iterator for ColumnIterator<'a> {
         }
 
         self.column += 1;
+
+        Some(v)
+    }
+}
+
+
+struct BlockIterator<'a> {
+    grid: &'a Grid,
+    row: u8,
+    column: u8,
+}
+
+impl<'a> BlockIterator<'a> {
+    fn new(grid: &'a Grid) -> BlockIterator<'a> {
+        BlockIterator { grid: grid, row: 0, column: 0 }
+    }
+}
+
+impl<'a> Iterator for BlockIterator<'a> {
+    type Item = Vec<&'a Slot>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.column >= 9 {
+            self.column = 0;
+            self.row += 3;
+        }
+        if self.row >= 9 {
+            return None;
+        }
+
+        let mut v = vec![];
+        for i in 0..3 {
+            for j in 0..3 {
+                v.push(&self.grid[self.row as usize + i][self.column as usize + j]);
+            }
+        }
+
+        self.column += 3;
 
         Some(v)
     }
@@ -90,6 +127,10 @@ impl SudokuPuzzle {
 
     fn columns(&self) -> ColumnIterator {
         ColumnIterator::new(&self.grid)
+    }
+
+    fn blocks(&self) -> BlockIterator {
+        BlockIterator::new(&self.grid)
     }
 
     fn print(&self) {
@@ -146,5 +187,21 @@ fn main() {
             Slot::Filled(n) => print!("{}, ", n),
             Slot::Empty => print!("_, "),
         }
+    }
+
+    println!("");
+    println!("");
+    println!("");
+
+    let blocks = puzzle.blocks();
+
+    for block in blocks {
+        for &slot in block {
+            match slot {
+                Slot::Filled(n) => print!("{}, ", n),
+                Slot::Empty => print!("_, "),
+            }
+        }
+        println!("")
     }
 }
