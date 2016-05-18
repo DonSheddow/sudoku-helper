1a2e2d61d79cf223f2bdf2eed8a2be2a2363b399
Sigurd Kolltveit <sigurd.kolltveit@gmx.com>
2016-05-18 02:10:00 +0200

    Add functions to iterate over rows and columns

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,42 @@ use std::env;
 use std::fs::File;
 use std::io::BufReader;
 
+type Grid = [[Slot; 9]; 9];
+
+type RowIterator<'a> = ::std::slice::Iter<'a, [Slot; 9]>;
+
+
+struct ColumnIterator<'a> {
+    grid: &'a Grid,
+    column: u8,
+}
+
+impl<'a> ColumnIterator<'a> {
+    fn new(grid: &'a Grid) -> ColumnIterator<'a> {
+        ColumnIterator { grid: grid, column: 0}
+    }
+}
+
+impl<'a> Iterator for ColumnIterator<'a> {
+    type Item = Vec<&'a Slot>;
+
+    fn next(&mut self) -> Option<Vec<&'a Slot>> {
+
+        if self.column >= 9 {
+            return None;
+        }
+
+        let mut v = vec![];
+        for row in 0..9 {
+            v.push(&self.grid[row as usize][self.column as usize]);
+        }
+
+        self.column += 1;
+
+        Some(v)
+    }
+}
+
 
 #[derive(Copy, Clone)]
 enum Slot {
@@ -14,7 +50,7 @@ enum Slot {
 }
 
 struct SudokuPuzzle {
-    grid: [[Slot; 9]; 9],
+    grid: Grid,
 }
 
 impl SudokuPuzzle {
@@ -48,12 +84,20 @@ impl SudokuPuzzle {
         Ok(puzzle)
     }
 
+    fn rows(&self) -> RowIterator {
+        self.grid.iter()
+    }
+
+    fn columns(&self) -> ColumnIterator {
+        ColumnIterator::new(&self.grid)
+    }
+
     fn print(&self) {
         let row_separator = ["-"; 9];
         let top_bot_sep = row_separator.join("-");
         println!("{}", top_bot_sep);
         let mut formatted_rows = vec![];
-        for row in self.grid.iter() {
+        for row in self.rows() {
             let mut str_row = "|".to_owned();
             for &slot in row.iter() {
                 let s = match slot {
@@ -69,7 +113,7 @@ impl SudokuPuzzle {
         print!("{}", formatted_rows.join(&between_sep));
         println!("{}", top_bot_sep);
     }
-
+    
 }
 
 
@@ -82,5 +126,25 @@ fn main() {
     };
 
     puzzle.print();
+    
+    let mut columns = puzzle.columns();
+    let first_column = columns.next().unwrap();
+    for &slot in first_column {
+        match slot {
+            Slot::Filled(n) => println!("{}", n),
+            Slot::Empty => println!("_"),
+        }
+    }
 
+    println!("");
+
+    let mut rows = puzzle.rows();
+    let first_row = rows.next().unwrap();
+    
+    for &slot in first_row {
+        match slot {
+            Slot::Filled(n) => print!("{}, ", n),
+            Slot::Empty => print!("_, "),
+        }
+    }
 }
