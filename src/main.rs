c0bff8c48fe0a342446bf086ac89065f3866e5eb
Sigurd Kolltveit <sigurd.kolltveit@gmx.com>
2016-05-25 00:55:46 +0200

    Create HTML/websocket-based GUI
    Add HTML-file with javascript to communicate with Rust via websockets.
    Add new constructor to create SudokuPuzzle from JSON.
    Add SudokuParseError enum to improve error handling.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+extern crate ws;
+extern crate serde_json;
+
+use ws::{listen, Sender, Handler, Message, Handshake};
+
 use std::io::stdin;
 use std::env;
 use std::fs::File;
@@ -7,7 +12,46 @@ mod sudoku;
 use sudoku::SudokuPuzzle;
 use sudoku::Slot;
 
+struct Server {
+    out: Sender,
+}
+
+impl Handler for Server {
+    fn on_message(&mut self, msg: Message) -> ws::Result<()> {
+        println!("Server got message '{}'", msg);
+        let s = msg.into_text().unwrap();
+
+        let puzzle = match SudokuPuzzle::from_json(&s) {
+            Ok(r) => r,
+            Err(e) => { return self.out.send(format!("{}", e)) },
+        };
+        let mut result = "".to_owned();
+        let mut columns = puzzle.columns();
+        let first_column = columns.next().unwrap();
+        for &slot in first_column {
+            let s = match slot {
+                Slot::Filled(n) => format!("{}, ", n),
+                Slot::Empty => "_, ".to_owned(),
+            };
+            result.push_str(&s);
+        }
+        self.out.send(result)
+    }
+
+    fn on_open(&mut self, shake: Handshake) -> ws::Result<()> {
+        println!("Got connection from {}", shake.remote_addr().unwrap().unwrap());
+        self.out.send("Hello, world!")
+    }
+
+}
+
 fn main() {
+
+
+    listen("127.0.0.1:3012", |out| {
+        Server { out: out }
+    }).unwrap();
+
 
     let stdin = stdin();
     let puzzle = match env::args().nth(1) {
--- src/sudoku.rs
+++ src/sudoku.rs
@@ -1,6 +1,12 @@
+extern crate serde_json;
+
+use serde_json::Value;
+
 use std::io::prelude::*;
-use std::error::Error;
+use std::error;
 use std::convert;
+use std::fmt;
+use std::num::ParseIntError;
 
 type Grid = [[Slot; 9]; 9];
 
@@ -97,13 +103,58 @@ pub enum Slot {
     Filled(u8),
 }
 
+#[derive(Debug)]
+pub enum SudokuParseError {
+    Json(serde_json::error::Error),
+    IntParsing(ParseIntError),
+    Syntax(&'static str),
+}
+
+impl fmt::Display for SudokuParseError {
+    fn fmt(&self, fmt: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            SudokuParseError::Json(ref error) => fmt::Display::fmt(error, fmt),
+            SudokuParseError::IntParsing(ref error) => fmt::Display::fmt(error, fmt),
+            SudokuParseError::Syntax(ref s) => fmt::Display::fmt(s, fmt),
+        }
+    }
+}
+
+impl error::Error for SudokuParseError {
+    fn description(&self) -> &str {
+        match *self {
+            SudokuParseError::Json(ref e) => error::Error::description(e),
+            SudokuParseError::IntParsing(ref e) => error::Error::description(e),
+            SudokuParseError::Syntax(s) => s,
+        }
+    }
+}
+
+impl From<serde_json::error::Error> for SudokuParseError {
+    fn from(error: serde_json::error::Error) -> SudokuParseError {
+        SudokuParseError::Json(error)
+    }
+}
+
+impl From<ParseIntError> for SudokuParseError {
+    fn from(error: ParseIntError) -> SudokuParseError {
+        SudokuParseError::IntParsing(error)
+    }
+}
+
+impl From<&'static str> for SudokuParseError {
+    fn from(error: &'static str) -> SudokuParseError {
+        SudokuParseError::Syntax(error)
+    }
+}
+
 pub struct SudokuPuzzle {
     grid: Grid,
 }
 
 impl SudokuPuzzle {
 
-    pub fn new<T>(mut file: T) -> Result<Self, Box<Error>>
+    pub fn new<T>(mut file: T) -> Result<Self, Box<error::Error>>
         where T: BufRead {
 
         let mut puzzle = SudokuPuzzle {
@@ -125,6 +176,38 @@ impl SudokuPuzzle {
                         return Err(convert::From::from("number must be between 1 and 9 inclusive"));
                     }
                     *slot = Slot::Filled(num);
+                }
+            }
+        }
+
+        Ok(puzzle)
+    }
+
+    pub fn from_json(json_str: &str) -> Result<Self, SudokuParseError> {
+        let json: Value = try!(serde_json::from_str(json_str));
+
+        let mut puzzle = SudokuPuzzle {
+            grid: [[Slot::Empty; 9]; 9],
+        };
+
+        let v = try!(json.as_array().ok_or("unable to interpret JSON as array"));
+        if v.len() != 9 {
+            return Err(convert::From::from("array must contain exactly 9 rows"));
+        }
+        for (row_idx, row_value) in v.iter().enumerate() {
+            let str_row = try!(row_value.as_string().ok_or("rows must be strings"));
+            let mut numbers = str_row.split(',');
+            for col_idx in 0..9 {
+                let str_num = try!(numbers.next().ok_or("not enough numbers in row")).trim();
+                if str_num == "_" {
+                    puzzle.grid[row_idx][col_idx] = Slot::Empty;
+                }
+                else {
+                    let num: u8 = try!(str_num.parse());
+                    if !(1 <= num && num <= 9) {
+                        return Err(convert::From::from("number must be between 1 and 9 inclusive"));
+                    }
+                    puzzle.grid[row_idx][col_idx] = Slot::Filled(num);
                 }
             }
         }
