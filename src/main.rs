c987a93c1fcfb80ad6e99df5641a1786f0dea1ad
Sigurd Kolltveit <sigurd.kolltveit@gmx.com>
2016-06-02 16:42:04 +0200

    Remove superfluous code

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,8 @@ extern crate ws;
 
 use ws::{listen, Sender, Handler, Message, Handshake};
 
-use std::io::stdin;
-use std::env;
-use std::fs::File;
-use std::io::BufReader;
-
 mod sudoku;
 use sudoku::SudokuPuzzle;
-use sudoku::Slot;
 
 struct Server {
     out: Sender,
@@ -39,53 +33,7 @@ impl Handler for Server {
 }
 
 fn main() {
-
-
     listen("127.0.0.1:3012", |out| {
         Server { out: out }
     }).unwrap();
-
-
-    let stdin = stdin();
-    let puzzle = match env::args().nth(1) {
-        Some(path) => SudokuPuzzle::from_file(BufReader::new(File::open(path).unwrap())).unwrap(),
-        None => SudokuPuzzle::from_file(stdin.lock()).unwrap(),
-    };
-
-    let mut columns = puzzle.columns();
-    let first_column = columns.next().unwrap();
-    for &slot in first_column {
-        match slot {
-            Slot::Filled(n) => println!("{}", n),
-            Slot::Empty => println!("_"),
-        }
-    }
-
-    println!("");
-
-    let mut rows = puzzle.rows();
-    let first_row = rows.next().unwrap();
-    
-    for &slot in first_row {
-        match slot {
-            Slot::Filled(n) => print!("{}, ", n),
-            Slot::Empty => print!("_, "),
-        }
-    }
-
-    println!("");
-    println!("");
-    println!("");
-
-    let blocks = puzzle.blocks();
-
-    for block in blocks {
-        for &slot in block {
-            match slot {
-                Slot::Filled(n) => print!("{}, ", n),
-                Slot::Empty => print!("_, "),
-            }
-        }
-        println!("")
-    }
 }
